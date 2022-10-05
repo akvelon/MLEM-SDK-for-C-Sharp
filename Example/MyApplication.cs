0ed6e98b56197a6062508c632eaef0c0c915b7b1
vladimir.krestov <vladimir.krestov@akvelon.com>
2022-10-05 20:36:44 +0400

    1. Reworked naming
    2. Resolved nullable reference types
    3. Little code refactoring

--- Example/MyApplication.cs
+++ Example/MyApplication.cs
@@ -15,29 +15,30 @@ namespace Example
             _mlemApiClientSvm = new MlemApiClient("http://127.0.0.1:8080/", logger);
         }
 
-        public async Task<List<long>> RunIris()
+        public async Task<List<long>?> RunIris()
         {
-            return await _mlemApiClientIris.PredictAsync<Iris, List<long>>(
-                new List<Iris>
+            List<Iris> testData = new()
+            {
+                new Iris
                 {
-                    new Iris
-                    {
-                        SepalLength = -69639435.20838484,
-                        SepalWidth = 64887767.01179123,
-                        PetalLength = -76043679.89193763,
-                        PetalWidth = 20142568.61724788
-                    },
-                    new Iris
-                    {
-                        SepalLength = 6343387.454046518,
-                        SepalWidth = -30195626.60490367,
-                        PetalLength = 64042930.90411937,
-                        PetalWidth = -69196204.98948716
-                    }
-                });
+                    SepalLength = -69639435.20838484,
+                    SepalWidth = 64887767.01179123,
+                    PetalLength = -76043679.89193763,
+                    PetalWidth = 20142568.61724788
+                },
+                new Iris
+                {
+                    SepalLength = 6343387.454046518,
+                    SepalWidth = -30195626.60490367,
+                    PetalLength = 64042930.90411937,
+                    PetalWidth = -69196204.98948716
+                }
+            };
+
+            return await _mlemApiClientIris.PredictAsync<Iris, List<long>>(testData);
         }
 
-        public async Task<List<double>> RunSvm()
+        public async Task<List<double>?> RunSvm()
         {
             return await _mlemApiClientSvm.PredictAsync<SvmModel, List<double>>(
                 new List<SvmModel>

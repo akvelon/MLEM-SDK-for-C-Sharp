5740aa2946d81411fae4fedd521dbdb31b844746
zoran.milovanovic <zoran.milovanovic@akvelon.com>
2023-03-02 16:49:33 +0100

    Add ArrayTypeValidator

---
+++ MlemApi/Validation/Validators/PrimitiveTypes/ArrayTypeValidator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using MlemApi.Dto;
+
+namespace MlemApi.Validation.Validators.PrimitiveTypes
+{
+    internal class ArrayTypeValidator : ITypeValidator
+    {
+        public List<Type> SupportedApiDescriptionDataStructure()
+        {
+            return new List<Type>() { typeof(ListData) };
+        }
+
+        public void Validate<T, D>(T value, IApiDescriptionDataStructure apiDescriptionDataStructure, ValidationParameters validationParameters = null, D? additionalData = null, ITypeValidator? childTypeVallidator = null)
+             where D : class?
+        {
+            var arrayData = apiDescriptionDataStructure as ListData;
+            var arrayValue = value as ICollection;
+
+            var arrayEnumerator = arrayValue.GetEnumerator();
+            arrayEnumerator.MoveNext();
+            var firstElement = arrayEnumerator.Current;
+
+            childTypeVallidator.Validate(firstElement, arrayData.Items.First(), validationParameters, additionalData, childTypeVallidator);
+        }
+    }
+}
